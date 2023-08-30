a9df9edb9b2626a6ccb9406f1575221b4dfbcff9
ss749236 <ss749236@gmail.com>
2023-08-30 15:24:07 +0530

    project

--- src/components/project.js
+++ src/components/project.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Projects } from "../data/projects";
 
 export default function Project() {
     return (
-        <div></div>
+        <section id="projects">
+            <div>
+                <div>
+                    <h1>Apps I've Built</h1>
+                </div>
+                <div className="project-map">
+                    {
+                        Projects.map((project) => (
+                            <a href={project.link}>
+                                <div>
+                                    <h2>{project.subtitle}</h2>
+                                    <h1>{project.title}</h1>
+                                </div>
+                            </a>
+                        ))
+                    }
+
+                </div>
+            </div>
+
+        </section>
     );
 }
---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+export const Projects = [
+    {
+        title: "E-commerce Website",
+        subtitle: "React Hooks",
+        link: "https://github.com/shivanisharma2895/ecommerce-app.git",
+        description: "Project is about how to built an E-commerce WEbsite using React hooks and also how to fetch APIs."
+    },
+    {
+        title: "Movie Search App",
+        subtitle: "API",
+        link: "https://github.com/shivanisharma2895/movie-search-app.git",
+        description: "This project provide information regarding latest movies."
+    },
+    {
+        title: "Weather App",
+        subtitle: "API, React Hooks, Axios",
+        link: "https://github.com/shivanisharma2895/weatherApp.git",
+        description: "Built by using Axios methode and providing real time weather updates."
+    },
+    {
+        title: "WhatsApp Clone",
+        subtitle: "TypeScript , SCSS",
+        link: "https://github.com/shivanisharma2895/whatsapp-clone.git",
+        description: "Built a clone of whatsapp using typescript and done styling using SASS."
+    }
+]
